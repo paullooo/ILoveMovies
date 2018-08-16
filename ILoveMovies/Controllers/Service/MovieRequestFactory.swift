e1a174ee01cbc786b641def565e2a1a2865f7eb8
Developer <dev.paulo.pereira@gmail.com>
2018-08-16 17:05:50 -0300

    Include prefix to change language request

--- ILoveMovies/Controllers/Service/MovieRequestFactory.swift
+++ ILoveMovies/Controllers/Service/MovieRequestFactory.swift
@@ -5,13 +5,15 @@
 //  Created by Developer on 11/08/18.
 //
 import Alamofire
+var prefix: String = NSLocalizedString("prefix", comment: "")
+
 class RequestFactory {
     static func getMovieSearch(with string: String) -> DataRequest {
-        let params: Parameters = ["api_key": apiKey, "language": "pt-BR", "query": string]
+        let params: Parameters = ["api_key": apiKey, "language": prefix, "query": string]
         return Alamofire.request(baseURL + "search/movie", method: .get, parameters: params)
     }
     static func getMovie(identifier: Int) -> DataRequest {
-        let params: Parameters = ["api_key": apiKey, "language": "pt-BR"]
+        let params: Parameters = ["api_key": apiKey, "language": prefix]
         return Alamofire.request(baseURL + "movie/\(identifier)", method: .get, parameters: params)
     }
     static func getImages(identifier: Int) -> DataRequest {
@@ -19,11 +21,11 @@ class RequestFactory {
         return Alamofire.request(baseURL + "movie/\(identifier)/images", method: .get, parameters: params)
     }
     static func getVideos(identifier: Int) -> DataRequest {
-        let params: Parameters = ["api_key": apiKey, "language": "pt-BR"]
+        let params: Parameters = ["api_key": apiKey, "language": prefix]
         return Alamofire.request(baseURL + "movie/\(identifier)/videos", method: .get, parameters: params)
     }
     static func getSimilarMovie(identifier: Int) -> DataRequest {
-        let params: Parameters = ["api_key": apiKey, "language": "pt-BR"]
+        let params: Parameters = ["api_key": apiKey, "language": prefix]
         return Alamofire.request(baseURL + "movie/\(identifier)/similar", method: .get, parameters: params)
     }
 }
